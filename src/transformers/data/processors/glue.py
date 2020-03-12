9e2bf841c3bcbc039e3c07af7dd80f006ffd4ee3
Mohith22 <mohithdamarapati@gmail.com>
2020-03-12 17:14:25 -0400

    Bug fix

--- src/transformers/data/processors/glue.py
+++ src/transformers/data/processors/glue.py
@@ -93,7 +93,7 @@ def glue_convert_examples_to_features(
             logger.info("Writing example %d/%d" % (ex_index, len_examples))
 
         inputs = tokenizer.encode_plus(example.text_a, example.text_b, add_special_tokens=True, max_length=max_length,)
-        input_ids, token_type_ids = inputs["input_ids"], torch.zeros(len(inputs))
+        input_ids, token_type_ids = inputs["input_ids"], [0]*len(inputs)
 
         # The mask has 1 for real tokens and 0 for padding tokens. Only real
         # tokens are attended to.
